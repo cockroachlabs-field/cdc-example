fe4425dc36bc14c71a778ff819feb8b5aa449928
Tim Veil <tjveil@gmail.com>
2018-12-02 09:32:09 -0500

    adding timeouts

--- run.cmd
+++ run.cmd
@@ -8,6 +8,8 @@ docker-compose start generator
 
 docker-compose start roach-backup
 
+TIMEOUT 10
+
 docker-compose exec roach-backup /cockroach/cockroach sql --insecure --execute="CREATE DATABASE ycsb_backup;"
 docker-compose exec roach-backup /cockroach/cockroach sql --insecure --database ycsb_backup --execute="CREATE TABLE usertable (ycsb_key INT8 NOT NULL, field1 STRING NULL, field2 STRING NULL, field3 STRING NULL, field4 STRING NULL, field5 STRING NULL, field6 STRING NULL, field7 STRING NULL, field8 STRING NULL, field9 STRING NULL, field10 STRING NULL)"
 
@@ -15,9 +17,11 @@ docker-compose exec roach-backup /cockroach/cockroach sql --insecure --database 
 docker-compose start zookeeper
 docker-compose start kafka
 
-
+TIMEOUT 10
 
 docker-compose exec kafka /usr/bin/kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic cdc
+
+TIMEOUT 10
 
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --database ycsb --execute="CREATE CHANGEFEED FOR TABLE usertable INTO 'kafka://localhost:9092';"
 
