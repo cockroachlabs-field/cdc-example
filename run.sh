abefc4d717eb10b1049ab2fc63ccb92cfdfc9709
Tim Veil <tjveil@gmail.com>
2018-12-03 15:42:01 -0500

    updating for proper active listener

--- run.sh
+++ run.sh
@@ -44,8 +44,10 @@ sleep 10
 
 # start changefeed
 docker-compose exec roach-source-0 /cockroach/cockroach sql --insecure --database ycsb --execute="CREATE CHANGEFEED FOR TABLE usertable INTO 'kafka://kafka:9092';"
-#docker-compose exec roach-source-0 /cockroach/cockroach sql --insecure --database ycsb --execute="CREATE CHANGEFEED FOR TABLE usertable INTO 'kafka://172.29.0.9:9092';"
 
+# use to show running jobs
+# docker-compose exec roach-source-0 /cockroach/cockroach sql --insecure --execute="SHOW JOBS;"
 
-
+# view kafka topic
+# docker-compose exec kafka /usr/bin/kafka-console-consumer --bootstrap-server=localhost:9092 --property print.key=true --from-beginning --topic=usertable
 
