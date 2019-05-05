bcba31f443e36b0a8248bdfa919aa42262993ca2
Tim Veil <tjveil@gmail.com>
2019-05-05 10:19:18 -0400

    creating schema

---
+++ common/src/main/java/io/crdb/cdc/common/DataTable.java
@@ -0,0 +1,23 @@
+package io.crdb.cdc.common;
+
+import java.util.UUID;
+
+public class DataTable {
+
+    private UUID id;
+
+    private Integer balance;
+
+    public DataTable(UUID id, Integer balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+}
